be0d682aa0acc2f107e2aa308b1ef654c3555a9b
Donggeon Lee <rocky2010aaa@gmail.com>
2021-09-29 09:54:57 +0900

    Update export-keystore

--- cmd/keys/export_keystore.go
+++ cmd/keys/export_keystore.go
@@ -1,12 +1,16 @@
 package keys
 
 import (
-	//"bufio"
-	//"github.com/cosmos/cosmos-sdk/client"
+	"bufio"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/client/input"
+	"github.com/cosmos/cosmos-sdk/crypto"
+	"github.com/cosmos/cosmos-sdk/crypto/keyring"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/konstellation/konstellation/crypto/keystore"
 	"github.com/spf13/cobra"
-	//"github.com/cosmos/cosmos-sdk/client/input"
-	//"github.com/cosmos/cosmos-sdk/client/keys"
-	//"github.com/konstellation/konstellation/crypto/keystore"
 )
 
 func exportKeyStoreCommand() *cobra.Command {
@@ -20,38 +24,41 @@ func exportKeyStoreCommand() *cobra.Command {
 }
 
 func runExportKeyStoreCmd(cmd *cobra.Command, args []string) error {
-	//kb, err := keys.NewKeyBaseFromHomeFlag()
-	//if err != nil {
-	//	return err
-	//}
+	inBuf := bufio.NewReader(cmd.InOrStdin())
+	keyringBackend, _ := cmd.Flags().GetString(flags.FlagKeyringBackend)
+	clientCtx, err := client.GetClientQueryContext(cmd)
+	if err != nil {
+		return err
+	}
+	kb, err := keyring.New(sdk.KeyringServiceName(), keyringBackend, clientCtx.HomeDir, inBuf)
+	if err != nil {
+		return err
+	}
 
-	//buf := bufio.NewReader(cmd.InOrStdin())
-	//clientCtx, err := client.GetClientQueryContext(cmd)
-	//if err != nil {
-	//	return err
-	//}
-	//decryptPassword, err := input.GetPassword("Enter passphrase to decrypt your key:", buf)
-	//if err != nil {
-	//	return err
-	//}
-	//encryptPassword, err := input.GetPassword("Enter passphrase to encrypt the exported key:", buf)
-	//if err != nil {
-	//	return err
-	//}
-	//
-	//ac, err := kb.ExportPrivateKeyObject(args[0], decryptPassword)
-	//if err != nil {
-	//	return err
-	//}
-	//
-	//clientCtx.Keyring.ExportPrivateKeyObject()
-	//
-	//clientCtx.Keyring.
-	//
-	//encryptedKeyStore, err := keystore.NewKeyStoreV3(ac, args[0], []byte(encryptPassword))
-	//
-	//cmd.Println(string(encryptedKeyStore))
-	//return err
+	decryptPassword, err := input.GetPassword("Enter passphrase to decrypt your key:", inBuf)
+	if err != nil {
+		return err
+	}
+	encryptPassword, err := input.GetPassword("Enter passphrase to encrypt the exported key:", inBuf)
+	if err != nil {
+		return err
+	}
+
+	ac, err := kb.ExportPrivKeyArmor(args[0], decryptPassword)
+	if err != nil {
+		return err
+	}
+	pk, _, err := crypto.UnarmorDecryptPrivKey(ac, decryptPassword)
+	if err != nil {
+		return err
+	}
+
+	encryptedKeyStore, err := keystore.NewKeyStoreV3(pk, args[0], []byte(encryptPassword))
+	if err != nil {
+		return err
+	}
+
+	cmd.Println(string(encryptedKeyStore))
 
 	return nil
 }
