43f77c6033b1048cdd1d19a3da973ee4de684bf9
shawayush <ayushshaw456@gmail.com>
2022-10-10 12:15:28 +0530

    Added Error Handling for main.go

--- cmd/knstld/main.go
+++ cmd/knstld/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"os"
 
+	"github.com/cosmos/cosmos-sdk/server"
 	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
 	"github.com/konstellation/konstellation/app"
 	"github.com/konstellation/konstellation/cmd/knstld/cmd/ibc"
@@ -27,6 +28,12 @@ func main() {
 	rootCmd.AddCommand(ibc.MigrateGenesisForIBC())
 
 	if err := svrcmd.Execute(rootCmd, app.DefaultNodeHome); err != nil {
-		os.Exit(1)
+		switch e := err.(type) {
+		case server.ErrorCode:
+			os.Exit(e.Code)
+
+		default:
+			os.Exit(1)
+		}
 	}
 }
